e2c89516e3e709682da33f2c78b52ba2da8194e3
Clarkie <andrew.t.clarke@gmail.com>
2017-05-21 22:59:22 +0100

    test: integration tests

---
+++ index.test.js
@@ -0,0 +1,43 @@
+import test from 'ava';
+import Hapi from 'hapi';
+
+import plugin from './index';
+
+test('plugin registered', async t => {
+  const server = new Hapi.Server();
+  server.connection({ port: 3000 });
+
+  server.route({ method: 'GET', path: '/', handler: (request, reply) => reply(request.app) });
+
+  await server
+    .register(plugin)
+    .then(() => server
+      .inject({ url: '/' })
+      .then(({ result }) => {
+        t.truthy(result.startTime);
+        t.truthy(result.endTime);
+      }));
+});
+
+test('plugin registered with all options', async t => {
+  const server = new Hapi.Server();
+  server.connection({ port: 3000 });
+
+  server.route({ method: 'GET', path: '/', handler: (request, reply) => reply(request.app) });
+
+  const options = {
+    enabled: false,
+    environmentName: 'integration',
+    region: 'ap-west-1',
+    metricsSentCallback: () => {},
+  };
+
+  await server
+    .register({ register: plugin, options })
+    .then(() => server
+      .inject({ url: '/' })
+      .then(({ result }) => {
+        t.truthy(result.startTime);
+        t.truthy(result.endTime);
+      }));
+});
