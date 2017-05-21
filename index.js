21090c84fe8d7cce6a7fde5a2055681b7907dc4d
Clarkie <andrew.t.clarke@gmail.com>
2017-05-21 15:37:13 +0100

    refactor: all the things and add eslint

---
+++ index.js
@@ -0,0 +1,21 @@
+const packageJSON = require('./package.json');
+const lifecycle = require('./lib/lifecycle');
+const metric = require('./lib/metric');
+
+exports.register = (server, options, next) => {
+  const responseMetric = metric.create(options);
+
+  server.ext('onRequest', lifecycle.setStartTime);
+  server.ext('onPreResponse', lifecycle.setEndTime);
+
+  server.on('response', (request) => {
+    const dimensions = metric.createDimensions(request);
+    responseMetric.put(request.app.end - request.app.start, 'responseTime', dimensions);
+  });
+
+  return next();
+};
+
+exports.register.attributes = {
+  pkg: packageJSON,
+};
